5dc23be8b630e00c4ceab150e950647304894005
Key Zhang <zmj1316@gmail.com>
2017-07-23 21:25:40 +0800

    Signed-off-by: Key Zhang <zmj1316@gmail.com>

--- light_ps.hlsl
+++ light_ps.hlsl
@@ -45,39 +45,39 @@ struct PixelInputType
 float4 LightPixelShader(PixelInputType input) : SV_TARGET
 {
 	float4 colors;
-float4 normals;
-float3 lightDir;
-float lightIntensity;
-float4 outputColor;
-
-float3 position;
-float3 pointDir;
-
-// Sample the colors from the color render texture using the point sampler at this texture coordinate location.
-colors = colorTexture.Sample(SampleTypePoint, input.tex);
-
-// Sample the normals from the normal render texture using the point sampler at this texture coordinate location.
-normals = normalTexture.Sample(SampleTypePoint, input.tex);
-
-position = positionTexture.Sample(SampleTypePoint, input.tex)*float3(1000,1000,1000);
-pointDir = position - lightPosition;
-// Invert the light direction for calculations.
-lightDir = -lightDirection;
-
-// Calculate the amount of light on this pixel.
-lightIntensity = saturate(dot(normals.xyz, lightDir));
-
-float4 diffuse = max(0, dot(normals, lightPosition - position));
-diffuse = diffuse * colors;
-
-float3 H = normalize(viewPosition - position + lightPosition - position);
-float4 specularColor = float4(0.1, 0.7, 0.1, 1);
-float4 specular = specularColor * pow(max(0, dot(H, normals)), 2);
-
-
-// Determine the final amount of diffuse color based on the color of the pixel combined with the light intensity.
-outputColor = saturate(colors * lightIntensity);
+	float4 normals;
+	float3 lightDir;
+	float lightIntensity;
+	float4 outputColor;
+	
+	float3 position;
+	float3 pointDir;
+	
+	// Sample the colors from the color render texture using the point sampler at this texture coordinate location.
+	colors = colorTexture.Sample(SampleTypePoint, input.tex);
+	
+	// Sample the normals from the normal render texture using the point sampler at this texture coordinate location.
+	normals = normalTexture.Sample(SampleTypePoint, input.tex);
+	
+	position = positionTexture.Sample(SampleTypePoint, input.tex)*float3(1000,1000,1000);
+	pointDir = position - lightPosition;
+	// Invert the light direction for calculations.
+	lightDir = -lightDirection;
+	
+	// Calculate the amount of light on this pixel.
+	lightIntensity = saturate(dot(normals.xyz, lightDir));
+	
+	float4 diffuse = max(0, dot(normals, lightPosition - position));
+	diffuse = diffuse * colors;
+	
+	float3 H = normalize(viewPosition - position + lightPosition - position);
+	float4 specularColor = float4(0.1, 0.7, 0.1, 1);
+	float4 specular = specularColor * pow(max(0, dot(H, normals)), 2);
+	
+	
+	// Determine the final amount of diffuse color based on the color of the pixel combined with the light intensity.
+	outputColor = saturate(colors * lightIntensity);
 	outputColor += specular;
 
-return outputColor;
+	return outputColor;
 }
